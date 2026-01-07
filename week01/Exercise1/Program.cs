dec514dd91d049f73115c79d0da4e9fc0576acfa
Dallin Layton <hyrumdrums@gmail.com>
2026-01-07 16:23:17 +0000

    add exersize 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // We use Console.Write to prompt the user for input, so the answer appears on the same line.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
