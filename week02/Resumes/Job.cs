039ee2817b339e48786d856a467881a1ee6d88af
Dallin Layton <hyrumdrums@gmail.com>
2026-01-07 22:00:12 +0000

    Add wk02 resume program

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+  public string _company;
+  public string _title;
+  public int _startYear;
+  public int _endYear;
+
+  public void Display()
+  {
+    Console.WriteLine($"{_title} ({_company}) {_startYear} - {_endYear}");
+  } 
+
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Valve";
+        job1._title = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Nintendo";
+        job2._title = "Game Developer";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Resume resume = new Resume();
+        resume._name = "Phillip Filop";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
