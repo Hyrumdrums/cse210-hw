c33a3e20064ef8c7479d09ca740b32695a1da90d
Dallin Layton <hyrumdrums@gmail.com>
2026-02-10 13:43:22 +0000

    Add shape W06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Rectangle("Red", 6, 7));
+        shapes.Add(new Square("Blue", 2));
+        shapes.Add(new Circle("Green", 500));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
